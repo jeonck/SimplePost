969b5f0eea2e03b0b4a9db62c4d002f8d53b89c2
metacogpe <metacog@kakao.com>
2024-04-03 22:39:36 +0900

    Initial commit

---
+++ src/main/java/org/metacog/simplepost/SimplePostApplication.java
@@ -0,0 +1,13 @@
+package org.metacog.simplepost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/metacog/simplepost/SimplePostApplicationTests.java
@@ -0,0 +1,13 @@
+package org.metacog.simplepost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
